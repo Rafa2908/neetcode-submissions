397ad598eccf392dd25597e7a40a89c200da152e
Jose Calderon <141576784+Rafa2908@users.noreply.github.com>
2026-09-18 20:58:52 -0400

    Add: validate-parentheses - submission-14

---
+++ Data Structures & Algorithms/validate-parentheses/submission-14.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        char[] arr = s.toCharArray();
+
+
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!stack.empty()
+                && (arr[i] == ')' && stack.peek() == '(' || arr[i] == '}' && stack.peek() == '{'
+                    || arr[i] == ']' && stack.peek() == '[')) {
+                stack.pop();
+            } else {
+                stack.push(arr[i]);
+            }
+        }
+
+        return stack.empty();
+    }
+}
