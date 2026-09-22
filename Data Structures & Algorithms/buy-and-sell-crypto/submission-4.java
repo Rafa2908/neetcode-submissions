eeba231f9abc69ee419cea1419068ad7647ab36a
Jose Calderon <141576784+Rafa2908@users.noreply.github.com>
2026-09-21 20:58:59 -0400

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int best = prices[0];
+
+        for(int i = 0; i < prices.length; i++){
+            if(best > prices[i]){
+                best = prices[i];
+            }
+
+            if(prices[i] > best){
+                int currentProfit = prices[i] - best;
+                if(currentProfit > profit){
+                    profit = currentProfit;
+                }
+            }
+        }
+
+        return profit;
+    }
+}
